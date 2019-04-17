e218f68b2fd4a3de6bc3f93456a111640e8d4613
MarlonGL <MarlonGL@users.noreply.github.com>
2019-04-17 20:28:43 -0300

    leitura do code

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ var arqPronto = false;
 var programCounter = 0;
 var atual;
 var leituraVariaveis = false;
+var leituraMetodos = false;
 var variaveis = [];
 var data = [];
+var metodos = [];
+var linhas = [];
 var teste = ".data";
+var halt = false;
+
+var ac;
+var n;
+var z;
 
 input.addEventListener('change', function(){
     //console.log(input.files);
@@ -16,7 +24,7 @@ input.addEventListener('change', function(){
         //.map(function(line){
            // return line.split(' ');
         //})
-        console.log(lines);
+        //console.log(lines);
         arqPronto = true;
     }
     reader.readAsText(input.files[0]);
@@ -29,63 +37,159 @@ function check(){
     if(atual.includes(".data"))
     {
         leituraVariaveis = true;
-        console.log(leituraVariaveis);
+        //console.log(leituraVariaveis);
+    }
+    if(atual.includes(".code"))
+    {
+        leituraMetodos = true;
+        //console.log(leituraMetodos);
+    }
+}
+
+function leituraDeVariaveis(){
+    while(leituraVariaveis === true){  
+        programCounter++;
+        atual = lines[programCounter];
+        if(!atual.includes(".enddata")){
+            //var nome = atual.split(fimv);
+            var teste = atual.split(' ');
+            var n = getBetween(teste[0], "", ":");
+            var pos = getBetween(teste[2], "#", ",");
+            if(pos == null){
+                console.log("ERRO NA LEITURA, CHEQUE AS POSICOES DE MEMORIA");
+                leituraDeVariaveis = false;
+                halt = true;
+            }
+            var v = teste[3].slice(1);
+            //console.log(n);
+            data.push({nome:n,posM:pos, valor:v});
+            /*var posm = teste[2].substr(0,3);
+            console.log(posm);*/
+            //variaveis.push(atual);
+        }
+        else
+        {
+            leituraVariaveis = false;
+            programCounter++;
+        }
+            
+    }
+}
+
+function leituraCode(){
+    while(leituraMetodos === true){
+        programCounter++;
+        atual = lines[programCounter];
+        if(!atual.includes(".endcode")){
+            if(atual.search(":")!== -1){
+                var n = getBetween(atual ,"", ":");
+                var teste = atual.split(' ');
+                metodos.push({nome:n, posM:programCounter});
+                var met = getBetween(atual ,": ", " ");
+                //var s = atual.split(/(?<=^\S+)\s/);
+                if(teste.length > 2){
+                    linhas.push({comando:teste[1], variavel:teste[2]});
+                }else{
+                    linhas.push({comando:teste[1], variavel:null});
+                }
+
+                //console.log(met);
+            }else{
+                var teste = atual.split(' ');
+                if(teste.length > 1){
+                    linhas.push({comando:teste[0], variavel:teste[1]});
+                }else{
+                    linhas.push({comando:teste[0], variavel:null});
+                }
+            }
+        }
+        else{
+            leituraMetodos = false;
+            //programCounter++;
+            halt = true;
+        }
     }
     
 }
-
-function read()
-{
+function read(){
     var terminou = false;
-    while(terminou === false)
-    {
+    while(terminou === false){
         atual = lines[programCounter];
-        console.log(typeof atual);
+        //console.log(typeof atual);
         //console.log(atual);
         check();
-        while(leituraVariaveis === true)
-        {  
-            programCounter++;
-            atual = lines[programCounter];
-            if(!atual.includes(".enddata"))
-            {
-                //var nome = atual.split(fimv);
-                var teste = atual.split(' ');
-                //console.log(teste);
-                var n = getBetween(teste[0], "", ":");
-                var pos = getBetween(teste[2], "#", ",");
-                var v = teste[3].slice(1);
-                //console.log(n);
-                data.push({nome:n,posM:pos, valor:v});
-
-                /*var posm = teste[2].substr(0,3);
-                console.log(posm);*/
-                //console.log(WordInBetween(atual,"", ":"));
-                //console.log(WordInBetween(atual, "DB", ","));
-                variaveis.push(atual);
-                //console.log(atual);
-            }
-            else
-            {
-                leituraVariaveis = false;
-            }
-                
+        if(leituraVariaveis === true){
+            leituraDeVariaveis();
         }
-        terminou = true;
-        //console.log(variaveis);
-        console.log(data);
+        if(leituraMetodos === true){
+            leituraCode();
+        }
+        if(halt){
+            terminou = true;
+            console.log(data);
+            console.log(metodos);
+            console.log(linhas);
+            run();
+        }
+        
 
     }
 }
 
-function data(varia){
-    for(var i; i< variaveis.length; i++)
-    {
-        //var varia = {'nome':variaveis[i], 'posMemo':0, 'valor':}
+function run(){
+    var rodando = true;
+    var l = 0;
+    var com;
+    var vari;
+    var objVar;
+    while(rodando){
+        com = linhas[l].comando;
+        vari = linhas[l].variavel;
+        //console.log(com, ' ', vari);
+        if(vari !== null){
+            //console.log('passou do null');
+            for(var i=0;i<data.length;i++){
+                //console.log(data[i].nome);
+                if(vari.includes(data[i].nome)){
+                    objVar = data[i];
+                    //console.log(objVar);
+                    break;
+                }
+            }
+        }
+        switch(com) {
+            case "LD":
+              ld(objVar.nome);
+              break;
+            case "ST":
+              // code block
+              break;
+            case "SUB":
+              // code block
+              break;
+            case "ADD":
+              // code block
+              break;
+            case "HALT":
+              // code block
+              break;
+            default:
+              // code block
+          }
+        l++;
+        if(l==linhas.length){
+            rodando = false;
+        }
     }
+    
+    
+    //checkMetodo(com);
+    //console.log(vari);
 
 }
-
+function checkMetodo(n){
+    
+}
 function getBetween(strSource, strStart, strEnd)
 {
     var Start, End;
@@ -101,14 +205,18 @@ function getBetween(strSource, strStart, strEnd)
     }
 }
 
-function WordInBetween(sentence, wordOne, wordTwo)
-        {
+function ld(v){
+    console.log(v);
+}
+function st(v){
 
-            var start = sentence.indexOf(wordOne) + wordOne.length + 1;
+}
+function sub(v){
 
-            var end = sentence.indexOf(wordTwo) - start - 1;
+}
+function add(v){
 
-            return sentence.substring(start, end);
+}
+function halt(){
 
-
-        }
+}
